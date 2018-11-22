e2e417ce8f1506f9e140411f0aae0a3cf068aafc
lbors <lucas_borges95@yahoo.com.br>
2018-11-22 00:01:54 -0200

    Update IR_Motor.ino

--- Testes/IR_Motor/IR_Motor.ino
+++ Testes/IR_Motor/IR_Motor.ino
@@ -1,23 +1,18 @@
 #include <Servo.h>
 
-//Servo
-Servo s; //cria o objeto servo para controle do servo 
-int pos = 0; // variavel de posição do servo
+Servo s; //cria o objeto servo para controle do servo
+int pos, pos_def = 0; // variavel de posição do servo
+boolean ida = true;
+boolean volta, controle = false;
 
-//Sensor ir
 int ir_sensor = 8;
-int objeto = 0;  
-
-int led = 4;
-int teste=13;
-boolean controle =false;
+int objeto = 0;
    
 void setup()  
 {
-  s.attach(6); //atribui o pino 9 ao servo
+  s.attach(6); //atribui o pino 6 ao servo
   pinMode(8, INPUT); //Pino ligado ao coletor do fototransistor
   pinMode(4, OUTPUT);
-  pinMode(13,OUTPUT);
   Serial.begin(9600);  
 }  
    
@@ -29,19 +24,11 @@ void loop()
 void sensor_ir(){
   objeto = digitalRead(ir_sensor); // realiza a leitura do sensor
   if (objeto == LOW){
-    controle=true;
-    digitalWrite(led,HIGH);
-    }
-/*    while(objeto==LOW){
-      digitalWrite(led, HIGH);
-      //s.write(pos);
-      objeto=digitalRead(ir_sensor);
-    }
-  }*/
-  else{
-    digitalWrite(led, LOW);
-    controle=false;
-    }
+    controle = true;
+    Serial.println("Detectado");
+  }
+  else
+    controle = false;
 }
 
 void motor(){
@@ -58,33 +45,42 @@ void motor(){
           digitalWrite(led,LOW);
         delay(30);
         }
-      delay (30);
-      for(pos=180; pos>=0; pos--){
-        s.write(pos);
-        sensor_ir();
-        if(controle==true){
-           digitalWrite(led,HIGH);
-           break;
-        }
-        else
-          digitalWrite(led,LOW);
-        delay(30);
-      }
-    case true:
+      delay (30void motor(){
+  if(ida == true){
+    for(pos = pos_def; pos < 180; pos++){
       s.write(pos);
-      break;
+      sensor_ir();
+      if(controle == true){
+        pos_def = pos;
+        Serial.println("Vai parar, foi detectado na ida");
+        break;
+      }
+      if(pos == 179 && controle == false){
+        ida = false;
+        volta = true;
+        Serial.println("Nada detectado na ida");
+        pos_def = pos;
+      }
+      delay(30);
+    }
   }
-/*  for(pos = 0; pos < 180; pos++)
-  {
-    s.write(pos);
-    sensor_ir();
-  delay(30);
+  
+  if(volta == true){
+    for(pos = pos_def; pos >= 0; pos--){
+      s.write(pos);
+      sensor_ir();
+      if(controle == true){
+        pos_def = pos;
+        Serial.println("Vai parar, foi detectado na volta");
+        break;
+      }
+      if(pos == 0 && controle == false){
+        volta = false;
+        ida = true;
+        Serial.println("Nada detectado na volta");
+        pos_def = pos;
+      }
+      delay(30);
+    }
   }
-delay(30);
-  for(pos = 180; pos >= 0; pos--)
-  {
-    s.write(pos);
-    sensor_ir();
-    delay(30);
-  }*/
 }
